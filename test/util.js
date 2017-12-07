a8a0d22fe67f5feee7ec3ef85c85f86a2abf978a
mkg20001 <mkg20001@gmail.com>
2017-12-07 16:42:38 +0100

    fix: Sink closed error (fixes #1)

--- test/util.js
+++ test/util.js
@@ -5,6 +5,8 @@ const Connection = require('interface-connection').Connection
 const multiaddr = require('multiaddr')
 const pull = require('pull-stream')
 const assert = require('assert')
+const TCP = require('libp2p-tcp')
+const tcp = new TCP()
 
 const Duplex = () => {
   const d = _Duplex()
@@ -16,8 +18,21 @@ const Duplex = () => {
   })
 }
 
+const TCPDuplex = cb => {
+  const server = tcp.createListener()
+  server.listen(multiaddr('/ip4/127.0.0.1/tcp/0'))
+  server.once('listening', () => {
+    server.getAddrs((err, addr) => {
+      if (err) return cb(err)
+      const client = tcp.dial(addr[0])
+      server.on('connection', sclient => cb(null, client, sclient))
+    })
+  })
+}
+
 module.exports = {
   Duplex,
+  TCPDuplex,
   pullCompare: (v, cb) => pull.collect((err, res) => {
     if (cb) {
       if (err) return cb(err)
